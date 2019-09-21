8dd0a205ba35a4e41151ba6c78f5db32e5152dc7
avishek <avishekzone@gmail.com>
2019-09-21 23:46:01 +0545

    Err fixed

--- server.js
+++ server.js
@@ -21,4 +21,4 @@ app.get("/count",(req,res)=>{
         
 })
 
-app.listen(3000, () => console.log(`Example app listening on port 3000!`))
+app.listen(3000||process.env.PORT, () => console.log(`Example app listening on port 3000!`))
