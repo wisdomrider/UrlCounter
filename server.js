ed7e4c5227bd5dc86d97ef315b49f8e3d78b0c87
avishek <avishekzone@gmail.com>
2019-09-21 23:39:07 +0545

    Err fixed

--- server.js
+++ server.js
@@ -20,6 +20,5 @@ app.get("/count",(req,res)=>{
   res.send("count added success")
         
 })
-const listener = app.listen(process.env.PORT||3000, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+
+app.listen(3000, () => console.log(`Example app listening on port 3000!`))
